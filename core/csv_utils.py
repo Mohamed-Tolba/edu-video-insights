90f348238c44d80b46ad9daecb6ea3b4eb0b79f3
Mohamed-Tolba <mohamed.demerdash93@gmail.com>
2025-06-24 20:34:41 +1000

    Creating a class to handle csv files

---
+++ core/csv_utils.py
@@ -0,0 +1,10 @@
+"""
+Module: csv_utils.py
+Author: Mohamed Tolba
+Date Created: 24-06-2025
+Last Updated: 24-06-2025
+
+Description:
+    Provides utility functions and classes for consistent reading, writing, and
+    managing CSV files throughout the edu-video-insights pipeline.
+"""
