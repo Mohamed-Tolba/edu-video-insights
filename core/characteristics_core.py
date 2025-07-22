2573c740ff1fc2176dff58a84d4cc157cd66e533
Mohamed-Tolba <mohamed.demerdash93@gmail.com>
2025-07-22 15:45:05 +1000

    updating to python3.13.5

--- core/characteristics_core.py
+++ core/characteristics_core.py
@@ -21,11 +21,11 @@ import sys
 import os
 import re # Regular expression module for text processing
 import cv2
-import moviepy as mp
-import numpy as np
-import scipy.io.wavfile as wav
-from scipy.signal import medfilt
-import matplotlib.pyplot as plt
+# import moviepy as mp
+# import numpy as np
+# import scipy.io.wavfile as wav
+# from scipy.signal import medfilt
+# import matplotlib.pyplot as plt
 
 from scenedetect import open_video, SceneManager 
 # open_video: Recommended function to load video files for analysis in PySceneDetect.
@@ -116,63 +116,6 @@ class CharacsExtractor:
         except Exception as e:
             print(f"❌ Unexpected error: {e}")
             return False
-    
-    def download_video_only(self, video_id, save_path="."):
-        """
-        Downloads video-only (without audio) from a YouTube video using yt_dlp.
-
-        Args:
-            video_id (str): The YouTube video ID.
-            save_path (str): The directory where the video will be saved.
-        """
-        video_url = self.MetadataExtractor_obj.construct_video_url(video_id)
-        if not video_url:
-            print(f"Could not retrieve video URL for video ID: {video_id}")
-            return
-        try:
-            ydl_opts = {
-                'outtmpl': f"{save_path}/%(id)s.%(ext)s", # Save the video with its ID as the filename
-                'noplaylist': True,  # Download only the single video, not the playlist
-                'quiet': True,  # Suppress output messages
-                'format': 'bv*'   # 'bv*' = best video-only stream (without audio)
-            }
-            ydl = yt_dlp.YoutubeDL(ydl_opts)
-            ydl.download([video_url])
-            return True
-        except Exception as e:
-            print(f"❌ Unexpected error: {e}")
-            return False
-    
-    def download_audio_only(self, video_id, save_path="."):
-        """
-        Downloads audio-only (without video) from a YouTube video using yt_dlp.
-
-        Args:
-            video_id (str): The YouTube video ID.
-            save_path (str): The directory where the video will be saved.
-        """
-        video_url = self.MetadataExtractor_obj.construct_video_url(video_id)
-        if not video_url:
-            print(f"Could not retrieve video URL for video ID: {video_id}")
-            return
-        try:
-            ydl_opts = {
-                'outtmpl': f"{save_path}/%(id)s.%(ext)s", # Save the video with its ID as the filename
-                'noplaylist': True,  # Download only the single video, not the playlist
-                'quiet': True,  # Suppress output messages
-                'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',   # You can change to 'wav', 'm4a', etc.
-                'preferredquality': '192',
-                }],
-                'format': 'bestaudio/best'
-            }
-            ydl = yt_dlp.YoutubeDL(ydl_opts)
-            ydl.download([video_url])
-            return True
-        except Exception as e:
-            print(f"❌ Unexpected error: {e}")
-            return False
 
     def count_scene_changes(self, video_id, video_path=".", threshold=10.0):
         """Detect and count scene changes in a video using PySceneDetect.
@@ -235,9 +178,157 @@ class CharacsExtractor:
         else:
             return None
 
-    def detect_tone_variability_from_video(self, video_path, temp_audio_path="temp_audio.wav"):
-        # Not working properly
+    def delete_file(self, file_path):
         """
+        Deletes a file if it exists.
+
+        Parameters:
+            file_path (str): Full path to the file you want to delete.
+
+        Returns:
+            bool: True if file was deleted, False if file did not exist.
+        """
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            return True  # File successfully deleted
+        else:
+            return False  # File did not exist    
+
+if __name__ == "__main__":
+    API_KEY = load_api_key(parent_dir + '/' + "keys/youtube_data_API_key.txt")  # Load the YouTube Data API key from the specified file
+    CharacsExtractor_obj = CharacsExtractor(API_KEY)  # Create an instance of the CharacsExtractor class with the API key
+    video_id = "sEE33_YIgys"  # Replace with an actual video ID for testing
+    # video_id = "sEE33_YIgys" # This video has some scene transitions
+    # MetadataExtractor_obj = MetadataExtractor(API_KEY)
+    # duration_sec = MetadataExtractor_obj.get_video_duration_sec(video_id)  # Get the video duration in seconds
+    # if duration_sec is not None:
+    #     print(f"Video duration for {video_id}: {duration_sec} seconds")
+    # else:
+    #     print(f"Could not retrieve duration for video {video_id}. Check if the video ID is correct or if the video exists.")
+    # 
+    # duration_min = CharacsExtractor_obj.get_video_duration_min(video_id)  # Get the video duration
+    # print(f"Video duration for {video_id}: {duration_min} minutes")  # Print the video duration
+# 
+    # word_count = CharacsExtractor_obj.count_words(video_id)  # Count the words in the video's transcript
+    # if word_count is not None:
+    #     print(f"Word count for video {video_id}: {word_count}")
+    # else:
+    #     print(f"No transcript available for video {video_id}.")
+# 
+    # speaking_speed = CharacsExtractor_obj.extract_word_per_minute(video_id)  # Calculate speaking speed
+    # if speaking_speed is not None:
+    #     print(f"Speaking speed for video {video_id}: {speaking_speed} words per minute")
+    # else:
+    #     print(f"Could not calculate speaking speed for video {video_id}. Check duration and word count.")
+
+
+    save_dir = parent_dir + '/temp'
+    CharacsExtractor_obj.download_youtube_video_audio(video_id, save_dir)  # Download the video
+    # CharacsExtractor_obj.download_video_only(video_id, save_dir)
+    # CharacsExtractor_obj.download_audio_only(video_id, save_dir)
+    video_path = os.path.join(save_dir, f"{video_id}.mp4")  # Construct the path to the downloaded video
+    temp_audio_path = os.path.join(save_dir, f"{video_id}.wav")
+    if os.path.exists(video_path):
+        print(f"Video downloaded successfully to {video_path}")
+    else:
+        print(f"Failed to download video {video_id}. Check if the video ID is correct or if the video exists.")
+    
+    # CharacsExtractor_obj.delete_file(video_path)
+    
+   
+    # Detect scene changes in the downloaded video
+    # total_scene_changes = CharacsExtractor_obj.count_scene_changes(video_id, video_path, threshold = 10.0)  # Set the threshold for scene change detection
+    # print(f"Total scene changes detected in {video_path}: {total_scene_changes}")
+    # scene_change_freq = CharacsExtractor_obj.extract_scene_change_freq(video_id, video_path, threshold = 10.0)  # Calculate scene change frequency
+    # if scene_change_freq is not None:
+    #     print(f"Scene change frequency for video {video_id}: {scene_change_freq} changes per minute")   
+    
+    # print(CharacsExtractor_obj.get_video_duration_sec(video_path))
+
+    # 
+    # print(video_url)
+    # # Download the video using pytube
+    # try: 
+    #     yt = YouTube(video_url)
+    #     print(yt.title)
+    #     print(yt.description)
+    #     print(yt.length)  # Video length in seconds
+    #     print(yt.author)
+# 
+    #     ys = yt.streams.get_highest_resolution()
+# 
+    #     print(ys)
+    #     ys.download()
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
+    # Example usage
+    # video_path = "example_video.mp4"
+    # extractor = CharacteristicsExtractor(video_path)
+    # extractor.extract_features()
+    # extractor.save_to_csv("new_characs.csv")
+    # print("Characteristics extracted and saved to new_characs.csv.")
+
+
+"""
+def download_video_only(self, video_id, save_path="."):
+        
+        Downloads video-only (without audio) from a YouTube video using yt_dlp.
+
+        Args:
+            video_id (str): The YouTube video ID.
+            save_path (str): The directory where the video will be saved.
+        
+        video_url = self.MetadataExtractor_obj.construct_video_url(video_id)
+        if not video_url:
+            print(f"Could not retrieve video URL for video ID: {video_id}")
+            return
+        try:
+            ydl_opts = {
+                'outtmpl': f"{save_path}/%(id)s.%(ext)s", # Save the video with its ID as the filename
+                'noplaylist': True,  # Download only the single video, not the playlist
+                'quiet': True,  # Suppress output messages
+                'format': 'bv*'   # 'bv*' = best video-only stream (without audio)
+            }
+            ydl = yt_dlp.YoutubeDL(ydl_opts)
+            ydl.download([video_url])
+            return True
+        except Exception as e:
+            print(f"❌ Unexpected error: {e}")
+            return False
+    
+    def download_audio_only(self, video_id, save_path="."):
+        
+        Downloads audio-only (without video) from a YouTube video using yt_dlp.
+
+        Args:
+            video_id (str): The YouTube video ID.
+            save_path (str): The directory where the video will be saved.
+        
+        video_url = self.MetadataExtractor_obj.construct_video_url(video_id)
+        if not video_url:
+            print(f"Could not retrieve video URL for video ID: {video_id}")
+            return
+        try:
+            ydl_opts = {
+                'outtmpl': f"{save_path}/%(id)s.%(ext)s", # Save the video with its ID as the filename
+                'noplaylist': True,  # Download only the single video, not the playlist
+                'quiet': True,  # Suppress output messages
+                'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',   # You can change to 'wav', 'm4a', etc.
+                'preferredquality': '192',
+                }],
+                'format': 'bestaudio/best'
+            }
+            ydl = yt_dlp.YoutubeDL(ydl_opts)
+            ydl.download([video_url])
+            return True
+        except Exception as e:
+            print(f"❌ Unexpected error: {e}")
+            return False
+
+def detect_tone_variability_from_video(self, video_path, temp_audio_path="temp_audio.wav"):
+        # Not working properly
         Estimates tone variability (in Hz) from an MP4 video file.
         Audio is extracted and analysed for pitch variability using zero-crossing rate.
 
@@ -247,8 +338,7 @@ class CharacsExtractor:
 
         Returns:
             float: Tone variability (standard deviation of pitch) in Hz.
-        """
-
+        
         # Step 1: Extract audio from the video using moviepy
         video_clip = mp.VideoFileClip(video_path)                           # Load video file
         video_clip.audio.write_audiofile(temp_audio_path)                   # Save extracted audio as WAV
@@ -321,93 +411,4 @@ class CharacsExtractor:
         # Expressive: >40 Hz.
         return round(tone_variability_hz, 3)
 
-    def delete_file(self, file_path):
-        """
-        Deletes a file if it exists.
-
-        Parameters:
-            file_path (str): Full path to the file you want to delete.
-
-        Returns:
-            bool: True if file was deleted, False if file did not exist.
-        """
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-            return True  # File successfully deleted
-        else:
-            return False  # File did not exist    
-
-if __name__ == "__main__":
-    API_KEY = load_api_key(parent_dir + '/' + "keys/youtube_data_API_key.txt")  # Load the YouTube Data API key from the specified file
-    CharacsExtractor_obj = CharacsExtractor(API_KEY)  # Create an instance of the CharacsExtractor class with the API key
-    video_id = "sEE33_YIgys"  # Replace with an actual video ID for testing
-    # video_id = "sEE33_YIgys" # This video has some scene transitions
-    # MetadataExtractor_obj = MetadataExtractor(API_KEY)
-    # duration_sec = MetadataExtractor_obj.get_video_duration_sec(video_id)  # Get the video duration in seconds
-    # if duration_sec is not None:
-    #     print(f"Video duration for {video_id}: {duration_sec} seconds")
-    # else:
-    #     print(f"Could not retrieve duration for video {video_id}. Check if the video ID is correct or if the video exists.")
-    # 
-    # duration_min = CharacsExtractor_obj.get_video_duration_min(video_id)  # Get the video duration
-    # print(f"Video duration for {video_id}: {duration_min} minutes")  # Print the video duration
-# 
-    # word_count = CharacsExtractor_obj.count_words(video_id)  # Count the words in the video's transcript
-    # if word_count is not None:
-    #     print(f"Word count for video {video_id}: {word_count}")
-    # else:
-    #     print(f"No transcript available for video {video_id}.")
-# 
-    # speaking_speed = CharacsExtractor_obj.extract_word_per_minute(video_id)  # Calculate speaking speed
-    # if speaking_speed is not None:
-    #     print(f"Speaking speed for video {video_id}: {speaking_speed} words per minute")
-    # else:
-    #     print(f"Could not calculate speaking speed for video {video_id}. Check duration and word count.")
-
-
-    save_dir = parent_dir + '/temp'
-    CharacsExtractor_obj.download_youtube_video_audio(video_id, save_dir)  # Download the video
-    # CharacsExtractor_obj.download_video_only(video_id, save_dir)
-    # CharacsExtractor_obj.download_audio_only(video_id, save_dir)
-    video_path = os.path.join(save_dir, f"{video_id}.mp4")  # Construct the path to the downloaded video
-    temp_audio_path = os.path.join(save_dir, f"{video_id}.wav")
-    if os.path.exists(video_path):
-        print(f"Video downloaded successfully to {video_path}")
-    else:
-        print(f"Failed to download video {video_id}. Check if the video ID is correct or if the video exists.")
-    
-    # CharacsExtractor_obj.detect_tone_variability_from_video(video_path, temp_audio_path)
-    # CharacsExtractor_obj.delete_file(video_path)
-    
-   
-    # Detect scene changes in the downloaded video
-    # total_scene_changes = CharacsExtractor_obj.count_scene_changes(video_id, video_path, threshold = 10.0)  # Set the threshold for scene change detection
-    # print(f"Total scene changes detected in {video_path}: {total_scene_changes}")
-    # scene_change_freq = CharacsExtractor_obj.extract_scene_change_freq(video_id, video_path, threshold = 10.0)  # Calculate scene change frequency
-    # if scene_change_freq is not None:
-    #     print(f"Scene change frequency for video {video_id}: {scene_change_freq} changes per minute")   
-    
-    # print(CharacsExtractor_obj.get_video_duration_sec(video_path))
-
-    # 
-    # print(video_url)
-    # # Download the video using pytube
-    # try: 
-    #     yt = YouTube(video_url)
-    #     print(yt.title)
-    #     print(yt.description)
-    #     print(yt.length)  # Video length in seconds
-    #     print(yt.author)
-# 
-    #     ys = yt.streams.get_highest_resolution()
-# 
-    #     print(ys)
-    #     ys.download()
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-    # Example usage
-    # video_path = "example_video.mp4"
-    # extractor = CharacteristicsExtractor(video_path)
-    # extractor.extract_features()
-    # extractor.save_to_csv("new_characs.csv")
-    # print("Characteristics extracted and saved to new_characs.csv.")
+"""
